de1cdb2cc6d373f411cf06e06edd10c39b172224
Alain Schlesser <alain.schlesser@gmail.com>
2018-03-10 21:31:15 +0100

    Provide fallback config values

--- src/Service/Credentials.php
+++ src/Service/Credentials.php
@@ -6,12 +6,19 @@ use Ripstop\Credentials as CredentialsObject;
 
 class Credentials
 {
+    const KEY_USERNAME = 'credentials.username';
+    const KEY_PASSWORD = 'credentials.password';
+
     public function get(): CredentialsObject
     {
         /** @var Config $config */
         $config   = Robo::service('config');
-        $username = $config->get('credentials.username');
-        $password = $config->get('credentials.password');
+        $username = $config->has(self::KEY_USERNAME)
+            ? $config->get(self::KEY_USERNAME)
+            : '<username>';
+        $password = $config->has(self::KEY_PASSWORD)
+            ? $config->get(self::KEY_PASSWORD)
+            : '<password>';
 
         return new CredentialsObject($username, $password);
     }
